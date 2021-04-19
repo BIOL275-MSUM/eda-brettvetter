a00e7196fccb474b5e32e7c9a3c129af60219693
Brett Vetter <brett.vetter@go.mnstate.edu>
2021-04-19 15:42:46 -0500

    Error when knitting readme.

--- EDA.R
+++ EDA.R
@@ -1,22 +1,83 @@
 
+
 # Load Packages -----------------------------------------------------------
 
 library(tidyverse)
 
 library(readxl)
 
+install.packages("corrplot")
+
+library(corrplot)
+
 # Read Data ---------------------------------------------------------------
-download_data <- read_excel("Data/2020 County Health Rankings Minnesota Data - v1_0 (3).xlsx", 
+ranked_meas_data <- read_excel("Data/2020 County Health Rankings Minnesota Data - v1_0 (3).xlsx", 
                             sheet= "Ranked Measure Data", skip = 1)
-download_data
-glimpse(download_data)
-transmute(
-  download_data,
-  FIPS,
-  State,
-  County,
-  Deaths,
-  HousingProbs = `% Severe Housing Problems`,
-  
-)
-LifeExpec = `Life Expectancy`
+ranked_meas_data
+glimpse(ranked_meas_data)
+
+ranked_meas_data <- 
+  transmute(
+    ranked_meas_data,
+    FIPS,
+    State,
+    County,
+    Deaths,
+    Housing_Probs = `% Severe Housing Problems`,
+    FP_Health = `% Fair or Poor Health`,
+    Smokers = `% Smokers`,
+    Mental_Unhealthy_Days = `Average Number of Mentally Unhealthy Days`,
+    Children_Poverty = `% Children in Poverty`
+  )
+
+add_meas_data <- read_excel("Data/2020 County Health Rankings Minnesota Data - v1_0 (3).xlsx",
+                               sheet = "Additional Measure Data", skip = 1)
+
+add_meas_data <-
+  transmute(
+    add_meas_data,
+    FIPS,
+    Life_Expec = `Life Expectancy`
+  )
+
+joined_data <- inner_join(ranked_meas_data, add_meas_data, by = "FIPS")
+
+joined_data
+
+# Plots ------------------------------------------------------------------
+
+joined_data %>%
+  filter(!is.na(County)) %>% 
+  select(-FIPS, -State, -County, -Deaths) %>% 
+  cor(use="na.or.complete") %>%
+  corrplot(method="circle")
+
+ggplot(joined_data, aes(x=Housing_Probs, y=FP_Health))+ geom_point()+
+  geom_smooth(method=lm)
+
+ggplot(joined_data, aes(x=Housing_Probs, y=Life_Expec))+ geom_point()+
+  geom_smooth(method=lm)
+
+ggplot(joined_data, aes(x=Smokers, y=FP_Health))+ geom_point()+
+  geom_smooth(method=lm)
+
+ggplot(joined_data, aes(x=Smokers, y=Life_Expec))+ geom_point()+
+  geom_smooth(method=lm)
+
+ggplot(joined_data, aes(x=Mental_Unhealthy_Days, y=FP_Health))+ geom_point()+
+  geom_smooth(method=lm)
+
+ggplot(joined_data, aes(x=Mental_Unhealthy_Days, y=Life_Expec))+ geom_point()+
+  geom_smooth(method=lm)
+
+ggplot(joined_data, aes(x=Housing_Probs, y=Mental_Unhealthy_Days))+ geom_point()+
+  geom_smooth(method=lm)
+
+ggplot(joined_data, aes(x=Children_Poverty, y=FP_Health))+ geom_point()+
+  geom_smooth(method=lm)
+
+ggplot(joined_data, aes(x=Children_Poverty, y=Life_Expec))+ geom_point()+
+  geom_smooth(method=lm)
+
+ggplot(joined_data, aes(x=Children_Poverty, y=Mental_Unhealthy_Days))+ geom_point()+
+  geom_smooth(method=lm)
